f1147bd4e29c006c78f66644d4e24189dd5584f0
Aimires <accsolverpost@gmail.com>
2023-03-05 12:01:53 +0100

    Start testing site, added run file

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main{
-	fmt.Println(a...: "Server running")
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	fmt.Println("Server running")
+
+	s := echo.New()
+
+	s.GET("/status", Handler)
+	err := s.Start(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+func Handler(ctx echo.Context) error {
+	err := ctx.String(http.StatusOK, "Hello world")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
